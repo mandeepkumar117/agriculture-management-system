90fa662927c4a43ba897037f0a4d975b83726b3a
Sanju chauhan <sanjuraja1805@gmail.com>
2025-04-25 16:22:17 +0530

    footer

--- frontend/agri_buzz/src/Layout.jsx
+++ frontend/agri_buzz/src/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Mynav5 from './Components/Mynavbar'
+import Myfooter from './Components/Myfooter'
 
 const Layout = () => {
   return (
     <>
     <div>
         <Mynav5/>
+        <Myfooter/>
     </div>
     <Outlet/>
     </>
